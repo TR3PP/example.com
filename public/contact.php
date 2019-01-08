8f75c205418131cd3fd5f0b14be77e2596888552
TR3PP <urt3@yahoo.com>
2019-01-08 11:02:15 -0600

    added mailgun

--- public/contact.php
+++ public/contact.php
@@ -1,78 +1,14 @@
 <?php
 
-class Validate{
-
-    public $validation = [];
-
-    public $errors = [];
-
-    private $data = [];
-
-    public function notEmpty($value){
-
-        if(!empty($value)){
-            return true;
-        }
-
-        return false;
-
-    }
-
-    public function email($value){
-
-        if(filter_var($value, FILTER_VALIDATE_EMAIL)){
-            return true;
-        }
-
-        return false;
-
-    }
-
-    public function check($data){
-
-        $this->data = $data;
-
-        foreach(array_keys($this->validation) as $fieldName){
-
-            $this->rules($fieldName);
-        }
-
-    }
-
-    public function rules($field){
-        foreach($this->validation[$field] as $rule){
-            if($this->{$rule['rule']}($this->data[$field]) === false){
-                $this->errors[$field] = $rule;
-            }
-        }
-    }
-
-    public function error($field){
-
-        if(!empty($this->errors[$field])){
-            //$s1 = $this->errors[$field]['message'];
-            //$s2 = $this->errors[$field]['rule'];
-            //$s3 = $s2 + " " + $s1;
-            //return $s3;
-            //echo $this->data[$field];
-            //return ($this->data[$field]);
-            return ($this->errors[$field]['message']);
-            
-        }
-
-        return false;
-    }
-
-    public function userInput($field){
-        return (!empty($this->data[$field])?$this->data[$field]:null);
-    }
-}
-
 require '../core/TR3PP/src/Validation/Validate.php';
+require '../vendor/autoload.php';
+require '../config/keys.php';
 use TR3PP\Validation;
+use Mailgun\Mailgun;
+
 
 $message = null;
-$valid = new TR3PP\Validation();
+$valid = new TR3PP\Validation\Validate();
 
 $args = [
     'name'=>FILTER_SANITIZE_STRING,
@@ -111,6 +47,26 @@ if(!empty($input)){
 
 
     if(empty($valid->errors)){
+
+        # Include the Autoloader (see "Libraries" for install instructions)
+        //require '../vendor/autoload.php';
+        //use Mailgun\Mailgun;
+
+        # Instantiate the client.
+        $mgClient = new Mailgun(MG_KEY);
+        $domain = MG_DOMAIN;
+
+        var_dump($input);
+
+        # Make the call to the client.
+        $result = $mgClient->sendMessage($domain, array(
+            'from'    => "{$input['name']} <{$input['email']}>",
+            'to'      => 'S Tripp <urt3@yahoo.com>',
+            'subject' => $input['subject'],
+            'text'    => $input['message']));
+
+        var_dump($result);
+
         $message = "<div class=\"message-success\">Your form has been submitted!</div>";
         //header('Location: thanks.php');
     }else{
